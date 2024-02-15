b58b3b6bc92c613067321df010d61a7a2ede5042
ckmridul <ckmkanad16@gmail.com>
2024-02-15 11:30:01 +0530

    add 7

--- ProjectX/settings.py
+++ ProjectX/settings.py
@@ -141,23 +141,23 @@ MEDIA_URL = "/media/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BAKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_BAKEND = os.getenv('EMAIL_BAKEND')
-EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_USE_TLS = True
-# EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
-EMAIL_PORT = 587
-# EMAIL_PORT = os.getenv('EMAIL_PORT')
-EMAIL_HOST_USER = "ckmridul77@gmail.com"
-# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = "nqjzickcpsaivjau"
-# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+# EMAIL_BAKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BAKEND = os.getenv('EMAIL_BAKEND')
+# EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+# EMAIL_USE_TLS = True
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+# EMAIL_PORT = 587
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+# EMAIL_HOST_USER = "ckmridul77@gmail.com"
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = "nqjzickcpsaivjau"
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 AUTH_USER_MODEL = "account.profile"
 
 
-KEY = "rzp_test_iY4JZpbTtJKYy6"
-# KEY = os.getenv('KEY')
-SECRET = "9kyDLAT2EOqeYbEu6DeoEtoS"
-# SECRET = os.getenv('SECRET')
+# KEY = "rzp_test_iY4JZpbTtJKYy6"
+KEY = os.getenv('KEY')
+# SECRET = "9kyDLAT2EOqeYbEu6DeoEtoS"
+SECRET = os.getenv('SECRET')
